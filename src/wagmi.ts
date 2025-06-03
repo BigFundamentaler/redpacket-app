690cae0dcaaa5b540da5b4cda10b7077db970a8c
bourne.xiao <bourne.xiao@corp.to8to.com>
2025-06-03 20:55:16 +0800

    fix: 添加合约读取功能

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -6,17 +6,19 @@ import {
   optimism,
   polygon,
   sepolia,
+  localhost,
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
+  projectId: 'd623c4e74d2b5605931df007b6f40df2',
   chains: [
     mainnet,
     polygon,
     optimism,
     arbitrum,
     base,
+    localhost,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
   ],
   ssr: true,
